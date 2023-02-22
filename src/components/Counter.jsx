1347fe1a0d135bca7116bd7c9e1aded6db661545
chihebchabou <chihebchaboutech@gmail.com>
2023-02-22 10:04:02 +0100

    refactoring to use redux hooks

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,30 +1,22 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import { increment, decrement, incrementByTen } from '../redux/actions/counterActions'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  increment,
+  decrement,
+  incrementByTen,
+} from '../redux/actions/counterActions';
 
-const Counter = ({counter, inc, dec, incByTen}) => {
+const Counter = () => {
+  const counter = useSelector(state => state.counterReducer.counter);
+  const dispatch = useDispatch();
   return (
     <div>
-      <button onClick={() => inc()}>+</button>
+      <button onClick={() => dispatch(increment())}>+</button>
       <span>{counter}</span>
-      <button onClick={() => dec()}>-</button>
-      <button onClick={() => incByTen()}>by ten</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByTen())}>by ten</button>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => {
-  return {
-    counter: state.counterReducer.counter
-  }
-}
-
-const mapDispatchToProps = disaptch => {
-  return {
-    inc : () => disaptch(increment()),
-    dec: () => disaptch(decrement()),
-    incByTen: () => disaptch(incrementByTen())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+export default Counter;
